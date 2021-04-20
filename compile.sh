13d107c6d070ca1b7466a3416c165f9abf6bddab
g84180098 <george.kaye@huawei.com>
2021-04-20 16:33:27 +0000

    Refining compilation script

--- compile.sh
+++ compile.sh
@@ -1,13 +1,69 @@
 #!/bin/bash
 
+DIR="/home/gkaye/circuits-cj"
+IMPORTS="$DIR/pkg.cfg1"
+OBJS=""
+
 rm -rf build/
-rm -rf bin
+rm -rf bin/
 mkdir build
 mkdir bin
+rm $IMPORTS
+touch $IMPORTS
 
-# hypergraphs
-mkdir build/hypergraphs
-cjc -c /home/gkaye/circuits-cj/src/hypergraphs/hypergraphs.cj -o build/hypergraphs/hypergraphs.o
+compile_object() {
+    echo "Building object $2/$1.o"
+    BUILD_DIR="$DIR/build/$2"
+    if [ ! -d $BUILD_DIR ] ; then
+        mkdir $BUILD_DIR
+    fi
+    BASE="$BUILD_DIR/$1"
+    OBJ="$BASE.o"
+    COMMAND="cjc -import-config $IMPORTS -c $DIR/src/$2/$1.cj -o $OBJ"
+    OBJS="$OBJS $OBJ"
+    echo $COMMAND
+    $COMMAND
+    CODE=$?
+    if [ "$CODE" != "0" ] ; then
+        echo "Error $CODE, aborting..."
+        exit 1
+    fi
+    CJO="$BASE.cjo"
+    printf "$1=$CJO\n" >> $IMPORTS
+}
 
-# main
-cjc -import-config pkg.cfg1 build/hypergraphs/hypergraphs.o src/main.cj -o bin/main.out
+compile_exec() { 
+    echo $OBJS
+    COMMAND="cjc -import-config $IMPORTS $OBJS $DIR/src/$1.cj -o $DIR/bin/$1.out"
+    echo $COMMAND
+    $COMMAND
+    CODE=$?
+    if [ "$CODE" != "0" ] ; then
+        echo "Error $CODE, aborting..."
+        exit 1
+    fi
+}
+
+compile_all_objects() {
+    for FILE in * ; do
+        if [ "$FILE" != "main.cj" ] ; then
+            if [[ -d $FILE ]] ; then
+                cd $FILE
+                if [ "$1" != "" ] ; then
+                    PKG=$1/$FILE
+                else 
+                    PKG=$FILE
+                fi 
+                compile_all_objects $PKG
+                cd ..
+            else 
+                FILE="${FILE%.*}"
+                compile_object $FILE $1 
+            fi
+        fi
+    done
+}
+
+compile_object "hypergraph" "hypergraphs"
+compile_object "components" "hypergraphs"
+compile_exec "main"
