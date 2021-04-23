d955df260ae5aea13d1148d2a606b3f3f61657af
g84180098 <george.kaye@huawei.com>
2021-04-23 08:46:56 +0000

    And gate component example completed

--- compile.sh
+++ compile.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-DIR="/home/gkaye/circuits-cj"
-IMPORTS="$DIR/import.conf"
+CANGJIE_ROOT=$1
+CORE="$CANGJIE_ROOT/build/build/modules/core/core.cjo"
+
+if [ -f "$CORE" ] ; then
+    echo "Found core library at $CORE, continuing..."
+else 
+    echo "Core library not found at $CORE, aborting..."
+    exit 1
+fi
+
+echo
+
+IMPORTS="import.conf"
 OBJS=""
 
 rm -rf build/
@@ -11,40 +22,12 @@ mkdir bin
 rm $IMPORTS
 touch $IMPORTS
 
-printf "core=/home/gkaye/arklang-sync/build/build/modules/core/core.cjo\n" >> $IMPORTS
-
-compile_object() {
-    if [ "$2" != "" ] ; then
-        BUILD_DIR="$DIR/build/$2"
-        SRC_DIR="$DIR/src/$2"
-        if [ ! -d $BUILD_DIR ] ; then
-            mkdir $BUILD_DIR
-        fi
-    else
-        BUILD_DIR="$DIR/build"
-        SRC_DIR="$DIR/src"
-    fi
-        
-    BASE="$BUILD_DIR/$1"
-    echo "Building object $BASE.o"
-    OBJ="$BASE.o"
-    COMMAND="cjc -import-config $IMPORTS -c $SRC_DIR/$1.cj -o $OBJ"
-    OBJS="$OBJS $OBJ"
-    echo $COMMAND
-    $COMMAND
-    CODE=$?
-    if [ "$CODE" != "0" ] ; then
-        echo "Error $CODE, aborting..."
-        exit 1
-    fi
-    CJO="$BASE.cjo"
-    printf "$1=$CJO\n" >> $IMPORTS
-}
+printf "core=$CORE\n" >> $IMPORTS
 
 compile_package() {
-    echo "Building package $1"
-    BUILD_DIR="$DIR/build/$1"
-    SRC_DIR="$DIR/src/$1"
+    echo "Building package $1..."
+    BUILD_DIR="build/$1"
+    SRC_DIR="src/$1"
     mkdir $BUILD_DIR
     COMMAND="cjc -import-config $IMPORTS -c -p $SRC_DIR -o $BUILD_DIR" 
     echo $COMMAND
@@ -58,12 +41,13 @@ compile_package() {
     CJO=$BUILD_DIR/$1.cjo
     OBJS="$OBJS $OBJ"
     printf "$1=$CJO\n" >> $IMPORTS
+    echo
 }
 
 compile_exec() { 
-    SRC="$DIR/src/$1.cj"
-    echo "Building executable $SRC"
-    COMMAND="cjc -import-config $IMPORTS $OBJS $DIR/src/$1.cj -o $DIR/bin/$1.out"
+    echo "Building executable $1..."
+    SRC="src/$1.cj"
+    COMMAND="cjc -import-config $IMPORTS $OBJS src/$1.cj -o bin/$1.out"
     echo $COMMAND
     $COMMAND
     CODE=$?
@@ -71,26 +55,7 @@ compile_exec() {
         echo "Error $CODE, aborting..."
         exit 1
     fi
-}
-
-compile_all_objects() {
-    for FILE in * ; do
-        if [ "$FILE" != "main.cj" ] ; then
-            if [[ -d $FILE ]] ; then
-                cd $FILE
-                if [ "$1" != "" ] ; then
-                    PKG=$1/$FILE
-                else 
-                    PKG=$FILE
-                fi 
-                compile_all_objects $PKG
-                cd ..
-            else 
-                FILE="${FILE%.*}"
-                compile_object $FILE $1 
-            fi
-        fi
-    done
+    echo
 }
 
 compile_package "hypergraphs"
