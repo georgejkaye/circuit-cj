962e44ca4c9b38f85cc585e21362569ac4babf58
g84180098 <george.kaye@huawei.com>
2021-05-11 16:41:30 +0000

    Making sure the adder runs for n > 2

--- run.sh
+++ run.sh
@@ -9,8 +9,7 @@ if [ ! -d $DOT ] ; then
     mkdir $DOT
 fi
 
-rm -f $DOT/$EXE.dot*
-touch $DOT/$EXE.dot
+rm -f $DOT/*
 
 # run executable
 echo "Running $BIN/$EXE.out"
@@ -21,11 +20,18 @@ if [ "$CODE" != "0" ] ; then
     exit 1
 fi
 
-# draw graph
-echo "Drawing svg graph from $DOT/$EXE.dot"
-dot -Tsvg $DOT/$EXE.dot -o $DOT/$EXE.dot.svg
-CODE=$?
-if [ "$CODE" != "0" ] ; then
-    echo "Error $CODE, aborting..."
-    exit 1
-fi
+# draw graphs
+
+cd $DOT
+
+for FILE in *; do 
+    echo "Drawing svg graph from $FILE"
+    dot -Tsvg $FILE -O
+    CODE=$?
+    if [ "$CODE" != "0" ] ; then
+        echo "Error $CODE, aborting..."
+        exit 1
+    fi
+done
+
+cd $ROOT
